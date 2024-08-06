9dac4f1744ea37563ff010341b326c31fcf0c27a
Mohammad Tabrez Alam <tabrezamrath@gmail.com>
2024-08-06 20:25:08 +0530

    New Page Creation and added some features

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("Api - Key")
+    GMSServices.provideAPIKey("AIzaSyCwXx0YGObBuriImmuCo8f0OpCZUGSPrU8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
